8cf95aa7556c80cad16ee343043b43cf565551ba
maudes <maudeshen@gmail.com>
2025-07-03 19:08:53 +0800

    Update: add receive times for 3

--- basic.py
+++ basic.py
@@ -2,24 +2,26 @@
 import socket
 import json
 
-# print('Hello World!')
+# WiZ specific info for connection
+wiz_ip = '192.168.0.21'  # Change based on the WiZ device you're testing
+wiz_port = 38899          # Fixed port WiZ using
 
-wiz_ip = "192.168.0.21"  # Should change to input for receiving light's IP
-wiz_port = 38899          # wiz port
-
-# 建立 UDP socket
+# Build up UDP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.settimeout(3)    # 3 seconds the top
+sock.settimeout(3)
 
-# 傳送 getPilot
-msg = {"method": "getPilot", "env": "pro"}
+# Prepare getPilot msg
+msg = {"method": "getPilot", "env": "prod"}
+# Send out the msg to WiZ lights
 sock.sendto(json.dumps(msg).encode(), (wiz_ip, wiz_port))
 
-# 等待回應
-try:
-    data, _ = sock.recvfrom(1024)
-    print("WiZ response：", json.loads(data.decode()))
-except socket.timeout:
-    print("No response")
+# Wait to receive Response from the network
+for _ in range(3):
+    try:
+        data, _ = sock.recvfrom(1024)
+        print('WiZ response: ', json.loads(data.decode()))
+    except socket.timeout:
+        print('No response')
 
+# Close the connection
 sock.close()
