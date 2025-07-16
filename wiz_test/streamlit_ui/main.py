075625daf2a1b265526743e60db13781ba1650bf
maudes <maudeshen@gmail.com>
2025-07-16 15:17:55 +0800

    Feat: streamlit ui

--- wiz_test/streamlit_ui/main.py
+++ wiz_test/streamlit_ui/main.py
@@ -1,3 +1,11 @@
 import streamlit as st
 
 st.title("Run local test with my WiZ light!")
+
+ip = st.text_input("Input the device IP address", "xxx.xxx.xxx.xxx")
+
+device_type = st.selectbox(
+    "What device type is your light?",
+    ("Color light", "Tuneable white light", "Dimmable light"),
+)
+st.write("You selected:", device_type)
