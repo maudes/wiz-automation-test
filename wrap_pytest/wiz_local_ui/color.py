f3d62c4a1b1fe011df2921294e84d3deb4b12cf5
maudes <maudeshen@gmail.com>
2025-07-16 15:00:29 +0800

    Delete wrap_pytest directory

--- wrap_pytest/wiz_local_ui/color.py
+++
@@ -1,2 +0,0 @@
-
-# common features: change RGB, lightmodes: 1 - 5, 7, 8, 19 - 28 (brightness, speed)
--- wrap_pytest/wiz_local_ui/temperature.py
+++
@@ -1,3 +0,0 @@
-
-# common features: change temperate 2700 - 6500
-# # change temperature lightmodes: 6, 11-16, 18 (brightness, speed)
